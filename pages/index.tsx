dbdfa68cfb4cbfac8b259935ad6750bb2cdad2da
Darren Jansson <darren.jansson@cinch.co.uk>
2023-05-10 14:17:09 +0100

    feat: add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
+import Head from 'next/head';
 import styles from "@/pages/index.module.css";
 import { Typography } from "@mui/material";
 import Container from '@mui/material/Container';
 
 export default function Home() {
   return (
-    <Container maxWidth="lg" className={styles.container}>
-      <Typography variant="h4" component="h1">GitHub Trending</Typography>
-      <Typography variant="body1">The most starred repositories created in the last 7 days!</Typography>
-    </Container>
+    <>
+      <Head>
+        <title>GitHub Trending | Darren Jansson</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container maxWidth="lg" className={styles.container}>
+        <Typography variant="h4" component="h1">GitHub Trending</Typography>
+        <Typography variant="body1">The most starred repositories created in the last 7 days!</Typography>
+      </Container>
+    </>
   );
 }
