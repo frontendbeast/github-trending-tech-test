ef1bd903172a752eecf1a8fbe890dd8ea7a9bcca
Darren Jansson <darren.jansson@cinch.co.uk>
2023-05-10 14:11:39 +0100

    feat: add mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
 
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <CssBaseline />
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import styles from "@/pages/index.module.css";
+import { Typography } from "@mui/material";
+import Container from '@mui/material/Container';
 
 export default function Home() {
   return (
-    <div>
-      <h1>GitHub Trending</h1>
-    </div>
+    <Container maxWidth="lg" className={styles.container}>
+      <Typography variant="h4" component="h1">GitHub Trending</Typography>
+      <Typography variant="body1">The most starred repositories created in the last 7 days!</Typography>
+    </Container>
   );
 }
